7dd79a45c08134e0fa548cf62e3e57083da62e12
M P GIRISH KUMAR <girishbarath2007@gmail,com>
2021-02-24 11:16:24 +0530

    scdac

---
+++ app/src/main/java/com/example/musicplayer/ui/main/PageViewModel.java
@@ -0,0 +1,10 @@
+package com.example.musicplayer.ui.main;
+
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+public class PageViewModel extends ViewModel {
+}
